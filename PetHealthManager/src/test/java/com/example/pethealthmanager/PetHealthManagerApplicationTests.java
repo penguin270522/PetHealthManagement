ff0bc8ad4e4ce9ee748b151739cb07bd600e4bab
Hoàng Hùng <116814313+penguin270522@users.noreply.github.com>
2024-06-05 09:52:33 +0700

    init

---
+++ PetHealthManager/src/test/java/com/example/pethealthmanager/PetHealthManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pethealthmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHealthManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
