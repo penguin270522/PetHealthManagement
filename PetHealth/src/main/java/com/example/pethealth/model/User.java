340bb6b45e8e6586f82e137637cc6b3e2780c716
Hoàng Hùng <116814313+penguin270522@users.noreply.github.com>
2024-06-25 12:10:24 +0700

    upt cde 25/6

--- PetHealth/src/main/java/com/example/pethealth/model/User.java
+++ PetHealth/src/main/java/com/example/pethealth/model/User.java
@@ -1,4 +1,41 @@
 package com.example.pethealth.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Date birthday;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
 }
+
