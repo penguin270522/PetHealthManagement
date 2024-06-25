32e76cf2f6bd8e3abb46904e20f5f4e7c13ca894
Hoàng Hùng <116814313+penguin270522@users.noreply.github.com>
2024-06-25 12:10:24 +0700

    upt cde 25/6

--- PetHealth/src/main/java/com/example/pethealth/model/Role.java
+++ PetHealth/src/main/java/com/example/pethealth/model/Role.java
@@ -1,4 +1,17 @@
 package com.example.pethealth.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
 }
+
